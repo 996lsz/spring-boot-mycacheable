f9add526be94c999068383098d9be76b1cbc5615
lsz <648748030@qq.com>
2019-12-17 10:50:21 +0800

    1.0
    更新注解

--- src/main/java/cn/lsz/mycacheable/annotation/MyCacheable.java
+++ src/main/java/cn/lsz/mycacheable/annotation/MyCacheable.java
@@ -13,13 +13,7 @@ import java.util.concurrent.TimeUnit;
 @Target({ ElementType.METHOD })
 @Retention(RetentionPolicy.RUNTIME)
 public @interface MyCacheable {
-
-    /**
-     * key实例
-     * 当前注解用于需要设置过期时间的缓存替代 @Cacheable
-     * 1、自动生成key,例如方法getList("param1","param2"),@RedisTemplateCache(key = "redis_key")生成的key为: redis_key:param1:param2
-     * 2、占位符key,例如方法getList("param1","param2"),@RedisTemplateCache(key = "redis_key_%s_%s")生成的key为: redis_key_param1_param2 ,注意占位符与参数个数对应
-     * */
+    
     String value();
 
     String[] key();
